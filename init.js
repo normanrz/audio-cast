a52401f80111c3342a693aaefb521a235cea62d4
Norman Rzepka <code@normanrzepka.de>
2013-10-19 22:29:26 -0700

    init

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
-/* Theater */
-
 global.DIR = process.env.PWD || __dirname;
 global.PORT = process.env.PORT || 5000;
+
+global.APP_KEY = process.env.APP_KEY;
 
 
 /* Application */
@@ -12,11 +12,18 @@ var app = require("./app/application.js");
 /* Routing */
 // Pages
 app.get("/receiver", function(req, res, next) {
-  res.render(__dirname + "/views/receiver.ejs");
+  res.render(__dirname + "/views/receiver.ejs", { appKey : process.env.APP_KEY });
 });
 
 app.get("/sender", function(req, res, next) {
-  res.render(__dirname + "/views/sender.ejs");
+  res.render(__dirname + "/views/sender.ejs", { appKey : process.env.APP_KEY });
+});
+
+app.get("/video.mp4", function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.sendfile("/static/video.mp4", {root : __dirname});
 });
 
 
